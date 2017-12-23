628f89a219ebc4e420b06cd47844f83b2816aa65
stobita <tobita.pc@gmail.com>
2017-12-23 18:25:27 +0900

    scss追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,53 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractSass = new ExtractTextPlugin("bundle.css");
 
-module.exports = {
-  entry: path.join(__dirname, 'src/app.jsx'),
-  output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js'
+module.exports = [
+  {
+    entry: path.join(__dirname, 'src/app.jsx'),
+    output: {
+      path: path.join(__dirname, 'www'),
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: 'www',
+      port: 4000,
+      inline: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
   },
-  devServer: {
-    contentBase: 'www',
-    port: 4000,
-    inline: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      }
+  {
+    entry: path.join(__dirname, 'src/styles/app.scss'),
+    output: {
+      path: path.join(__dirname, 'www'),
+      filename: 'bundle.css'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract(
+            {
+              fallback: "style-loader",
+              use: ["css-loader", "sass-loader"]
+            }
+          )
+        }
+      ]
+    },
+    plugins: [
+      extractSass
     ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
   }
-};
+];
